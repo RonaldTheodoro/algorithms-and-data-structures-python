7fb56ea5a303b8f4dd5f05b481eb9dd63cce0012
Ronald Theodoro <ronaldtheodoro@gmail.com>
2021-02-15 21:51:36 -0300

    Implement Stack using a dictionario to store itens

---
+++ algorithms/stacks/stack.py
@@ -0,0 +1,32 @@
+class Stack:
+
+    def __init__(self):
+        self._count = 0
+        self._items = {}
+
+    def push(self, element):
+        self._items[self._count] = element
+        self._count += 1
+
+    def pop(self):
+        if self.is_empty():
+            return None
+
+        self._count -= 1
+        return self._items.pop(self._count)
+
+    def peek(self):
+        if self.is_empty():
+            return None
+
+        return self._items[self._count - 1]
+
+    def is_empty(self):
+        return self._count == 0
+
+    def clear(self):
+        self._items = {}
+        self._count = 0
+
+    def __len__(self):
+        return self._count
