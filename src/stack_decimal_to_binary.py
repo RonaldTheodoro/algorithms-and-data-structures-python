6b410faabdfdc689141a76e05ceacd64bf65e884
Ronald Theodoro <ronaldtheodoro@gmail.com>
2021-02-15 22:25:22 -0300

    Add decimal_to_binary

---
+++ src/stack_decimal_to_binary.py
@@ -0,0 +1,16 @@
+from algorithms.stacks.stack import Stack
+
+
+def decimal_to_binary(decimal_number):
+    stack = Stack()
+    number = int(decimal_number)
+    binary_string = ''
+
+    while number > 0:
+        stack.push(number % 2)
+        number = number // 2
+
+    while not stack.is_empty():
+        binary_string += str(stack.pop())
+
+    return binary_string
---
+++ tests/src/test_decimal_to_binary.py
@@ -0,0 +1,7 @@
+from src.stack_decimal_to_binary import decimal_to_binary
+
+
+def test_decimal_to_binary():
+    assert decimal_to_binary(233) == '11101001'
+    assert decimal_to_binary(10) == '1010'
+    assert decimal_to_binary(1000) == '1111101000'
