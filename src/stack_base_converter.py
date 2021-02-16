d2f8c06066b4049e67fd845522b695273f5c30e8
Ronald Theodoro <ronaldtheodoro@gmail.com>
2021-02-15 22:35:33 -0300

    Add base_converter with tests

---
+++ src/stack_base_converter.py
@@ -0,0 +1,20 @@
+from algorithms.stacks.stack import Stack
+
+
+def base_converter(decimal_number, base):
+    stack = Stack()
+    digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    number = int(decimal_number)
+    base_string = ''
+
+    if not (base >= 2 and base <= 36):
+        return ''
+
+    while number > 0:
+        stack.push(number % base)
+        number = number // base
+
+    while not stack.is_empty():
+        base_string += digits[stack.pop()]
+
+    return base_string
---
+++ tests/src/test_base_converter.py
@@ -0,0 +1,9 @@
+from src.stack_base_converter import base_converter
+
+
+def test_base_converter():
+    assert base_converter(100345, 2) == '11000011111111001'
+    assert base_converter(100345, 8) == '303771'
+    assert base_converter(100345, 16) == '187F9'
+    assert base_converter(100345, 35) == '2BW0'
+    assert base_converter(100345, 40) == ''
