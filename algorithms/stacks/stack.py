88f46cdc550212746b08c6df0a9aa29aefedb23b
Ronald Theodoro <ronaldtheodoro@gmail.com>
2021-02-19 21:00:06 -0300

    fix typo

--- algorithms/stacks/stack.py
+++ algorithms/stacks/stack.py
@@ -2,10 +2,10 @@ class Stack:
 
     def __init__(self):
         self._count = 0
-        self._items = {}
+        self._itens = {}
 
     def push(self, element):
-        self._items[self._count] = element
+        self._itens[self._count] = element
         self._count += 1
 
     def pop(self):
@@ -13,26 +13,26 @@ class Stack:
             return None
 
         self._count -= 1
-        return self._items.pop(self._count)
+        return self._itens.pop(self._count)
 
     def peek(self):
         if self.is_empty():
             return None
 
-        return self._items[self._count - 1]
+        return self._itens[self._count - 1]
 
     def is_empty(self):
         return self._count == 0
 
     def clear(self):
-        self._items = {}
+        self._itens = {}
         self._count = 0
 
     def __len__(self):
         return self._count
 
     def __str__(self):
-        return f'Stack({[self._items[i] for i in range(self._count)]})'
+        return f'Stack({[self._itens[i] for i in range(self._count)]})'
 
     def __bool__(self):
         return not self.is_empty()
--- algorithms/stacks/stack_list.py
+++ algorithms/stacks/stack_list.py
@@ -1,34 +1,34 @@
 class StackList:
 
     def __init__(self):
-        self._items = []
+        self._itens = []
 
     def push(self, element):
-        self._items.append(element)
+        self._itens.append(element)
 
     def pop(self):
         if self.is_empty():
             return None
 
-        return self._items.pop()
+        return self._itens.pop()
 
     def peek(self):
         if self.is_empty():
             return None
 
-        return self._items[-1]
+        return self._itens[-1]
 
     def is_empty(self):
-        return len(self._items) == 0
+        return len(self._itens) == 0
 
     def clear(self):
-        self._items = []
+        self._itens = []
 
     def __len__(self):
-        return len(self._items)
+        return len(self._itens)
 
     def __str__(self):
-        return f'StackList({self._items})'
+        return f'StackList({self._itens})'
 
     def __bool__(self):
         return not self.is_empty()
