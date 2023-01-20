b9f2a169e87b6ca491b557154408c4cdd85e6519
Ronald Theodoro <ronaldtheodoro@gmail.com>
2023-01-19 23:36:59 -0300

    Make linked list moer pythonic

--- algorithms/linked_list/linked_list.py
+++ algorithms/linked_list/linked_list.py
@@ -8,41 +8,41 @@ class LinkedList:
             equals = default_equals
 
         # TODO: add _ in the instance properties
-        self.count = 0
-        self.head = None
-        self.equals = equals
+        self._count = 0
+        self._head = None
+        self._equals = equals
 
     def push(self, element):
         node = Node(element)
-        if self.head is None:
-            self.head = node
+        if self._head is None:
+            self._head = node
         else:
-            current = self.head
+            current = self._head
             while current.next is not None:
                 current = current.next
             current.next = node
-        self.count += 1
+        self._count += 1
 
     def insert(self, element, index):
-        if index >= 0 and index <= self.count: # TODO: move it to a private method
+        if index >= 0 and index <= self._count: # TODO: move it to a private method
             node = Node(element)
             if index == 0:
-                current = self.head
+                current = self._head
                 node.next = current
-                self.head = node
+                self._head = node
             else:
                 previous = self.get_element_at(index - 1)
                 current = previous.next
                 node.next = current
                 previous.next = node
-            self.count += 1
+            self._count += 1
             return True
         return False
 
     def get_element_at(self, index):
         # TODO: make get_element_at return the element not a node instance
-        if index >= 0 and index <= self.count: # TODO: move it to a private method
-            node = self.head
+        if index >= 0 and index <= self._count: # TODO: move it to a private method
+            node = self._head
             i = 0
             while i < index and node is not None:
                 node = node.next
@@ -55,50 +55,51 @@ class LinkedList:
         return self.remove_at(index)
 
     def index_of(self, element):
-        current = self.head
+        current = self._head
         i = 0
         # TODO: Transform while in for
-        while i < self.count and current is not None:
-            if self.equals(element, current.element):
+        while i < self._count and current is not None:
+            if self._equals(element, current.element):
                 return i
             current = current.next
             i += 1
         return -1
 
     def remove_at(self, index):
-        if index >= 0 and index < self.count: # TODO: move it to a private method
-            current = self.head
+        if index >= 0 and index < self._count: # TODO: move it to a private method
+            current = self._head
             if index == 0:
-                self.head = current.next
+                self._head = current.next
             else:
                 previous = self.get_element_at(index - 1)
                 current = previous.next
                 previous.next = current.next
-            self.count -= 1
+            self._count -= 1
             return current.element
         return None
 
     def is_empty(self):
-        return self.size() == 0
+        return len(self) == 0
 
-    def size(self):
-        # TODO: Transform it in __len__
-        return self.count
+    @property
+    def head(self):
+        return self._head
 
-    def get_head(self):
-        # TODO: Transform it in a property
-        return self.head
+    def __len__(self):
+        return self._count
 
-    def to_string(self):
-        # TODO: Transform it in __str__
-        if self.head is None:
+    def __str__(self):
+        if self._head is None:
             return ""
 
-        obj_string = f"{self.head.element}"
-        current = self.head.next
+        obj_string = f"{self._head.element}"
+        current = self._head.next
         i = 1
-        while i < self.size() and current is not None:
+        while i < len(self) and current is not None:
             obj_string = f"{obj_string},{current.element}"
             current = current.next
             i += 1
         return obj_string
+
+    def __bool__(self):
+        return len(self) > 0
--- tests/algorithms/linked_list/test_linked_list.py
+++ tests/algorithms/linked_list/test_linked_list.py
@@ -1,5 +1,7 @@
 # TODO: Add fixture with values already loaded
 import pytest
+
+
 @pytest.fixture
 def linked_list_with_elements(linked_list):
     linked_list.push(1)
@@ -7,59 +9,64 @@ def linked_list_with_elements(linked_list):
     linked_list.push(3)
     return linked_list
 
+
 def test_push_element(linked_list):
     assert linked_list.is_empty()
     linked_list.push(1)
     linked_list.push(2)
     linked_list.push(3)
-    assert linked_list.size() == 3
+    assert len(linked_list) == 3
 
 
 def test_insert_element_in_beginning(linked_list_with_elements):
-    assert linked_list_with_elements.size() == 3
+    assert len(linked_list_with_elements) == 3
     assert linked_list_with_elements.insert(0, 0)
     assert linked_list_with_elements.get_element_at(0).element == 0
     assert linked_list_with_elements.get_element_at(1).element == 1
     assert linked_list_with_elements.get_element_at(2).element == 2
     assert linked_list_with_elements.get_element_at(3).element == 3
-    assert linked_list_with_elements.size() == 4
+    assert len(linked_list_with_elements) == 4
 
 
 def test_insert_element_in_second_position(linked_list_with_elements):
-    assert linked_list_with_elements.size() == 3
+    assert len(linked_list_with_elements) == 3
     assert linked_list_with_elements.insert(0, 1)
     assert linked_list_with_elements.get_element_at(0).element == 1
     assert linked_list_with_elements.get_element_at(1).element == 0
     assert linked_list_with_elements.get_element_at(2).element == 2
     assert linked_list_with_elements.get_element_at(3).element == 3
-    assert linked_list_with_elements.size() == 4
+    assert len(linked_list_with_elements) == 4
 
 
 def test_insert_element_in_third_position(linked_list_with_elements):
-    assert linked_list_with_elements.size() == 3
+    assert len(linked_list_with_elements) == 3
     assert linked_list_with_elements.insert(0, 2)
     assert linked_list_with_elements.get_element_at(0).element == 1
     assert linked_list_with_elements.get_element_at(1).element == 2
     assert linked_list_with_elements.get_element_at(2).element == 0
     assert linked_list_with_elements.get_element_at(3).element == 3
-    assert linked_list_with_elements.size() == 4
+    assert len(linked_list_with_elements) == 4
 
 
 def test_insert_element_in_forth_position(linked_list_with_elements):
-    assert linked_list_with_elements.size() == 3
+    assert len(linked_list_with_elements) == 3
     assert linked_list_with_elements.insert(0, 3)
     assert linked_list_with_elements.get_element_at(0).element == 1
     assert linked_list_with_elements.get_element_at(1).element == 2
     assert linked_list_with_elements.get_element_at(2).element == 3
     assert linked_list_with_elements.get_element_at(3).element == 0
-    assert linked_list_with_elements.size() == 4
+    assert len(linked_list_with_elements) == 4
 
 
-def test_try_insert_element_in_invalid_position_before_begin(linked_list_with_elements):
+def test_try_insert_element_in_invalid_position_before_begin(
+    linked_list_with_elements,
+):
     assert not linked_list_with_elements.insert(0, -1)
 
 
-def test_try_insert_element_in_invalid_position_after_end(linked_list_with_elements):
+def test_try_insert_element_in_invalid_position_after_end(
+    linked_list_with_elements,
+):
     assert not linked_list_with_elements.insert(0, 99)
 
 
@@ -80,35 +87,37 @@ def test_try_get_element_at_before_beginning(linked_list_with_elements):
 
 
 def test_remove_first_element(linked_list_with_elements):
-    assert linked_list_with_elements.size() == 3
+    assert len(linked_list_with_elements) == 3
     assert linked_list_with_elements.remove(1) == 1
     assert linked_list_with_elements.get_element_at(0).element == 2
     assert linked_list_with_elements.get_element_at(1).element == 3
-    assert linked_list_with_elements.size() == 2
+    assert len(linked_list_with_elements) == 2
 
 
 def test_remove_second_element(linked_list_with_elements):
-    assert linked_list_with_elements.size() == 3
+    assert len(linked_list_with_elements) == 3
     assert linked_list_with_elements.remove(2) == 2
     assert linked_list_with_elements.get_element_at(0).element == 1
     assert linked_list_with_elements.get_element_at(1).element == 3
-    assert linked_list_with_elements.size() == 2
+    assert len(linked_list_with_elements) == 2
 
 
 def test_remove_third_element(linked_list_with_elements):
-    assert linked_list_with_elements.size() == 3
+    assert len(linked_list_with_elements) == 3
     assert linked_list_with_elements.remove(3) == 3
     assert linked_list_with_elements.get_element_at(0).element == 1
     assert linked_list_with_elements.get_element_at(1).element == 2
-    assert linked_list_with_elements.size() == 2
+    assert len(linked_list_with_elements) == 2
 
 
-def test_try_remove_non_existing_element_should_return_none(linked_list_with_elements):
+def test_try_remove_non_existing_element_should_return_none(
+    linked_list_with_elements,
+):
     assert linked_list_with_elements.remove(99) is None
     assert linked_list_with_elements.get_element_at(0).element == 1
     assert linked_list_with_elements.get_element_at(1).element == 2
     assert linked_list_with_elements.get_element_at(2).element == 3
-    assert linked_list_with_elements.size() == 3
+    assert len(linked_list_with_elements) == 3
 
 
 def test_index_of(linked_list_with_elements):
@@ -122,33 +131,33 @@ def test_index_of_with_non_existing_value(linked_list_with_elements):
 
 
 def test_remove_at_first_element(linked_list_with_elements):
-    assert linked_list_with_elements.size() == 3
+    assert len(linked_list_with_elements) == 3
     assert linked_list_with_elements.remove_at(0) == 1
-    assert linked_list_with_elements.size() == 2
+    assert len(linked_list_with_elements) == 2
 
 
 def test_remove_at_second_element(linked_list_with_elements):
-    assert linked_list_with_elements.size() == 3
+    assert len(linked_list_with_elements) == 3
     assert linked_list_with_elements.remove_at(1) == 2
-    assert linked_list_with_elements.size() == 2
+    assert len(linked_list_with_elements) == 2
 
 
 def test_remove_at_third_element(linked_list_with_elements):
-    assert linked_list_with_elements.size() == 3
+    assert len(linked_list_with_elements) == 3
     assert linked_list_with_elements.remove_at(2) == 3
-    assert linked_list_with_elements.size() == 2
+    assert len(linked_list_with_elements) == 2
 
 
 def test_remove_at_invalid_element_before_beginning(linked_list_with_elements):
-    assert linked_list_with_elements.size() == 3
+    assert len(linked_list_with_elements) == 3
     assert linked_list_with_elements.remove_at(-1) is None
-    assert linked_list_with_elements.size() == 3
+    assert len(linked_list_with_elements) == 3
 
 
 def test_remove_at_invalid_element_after_end(linked_list_with_elements):
-    assert linked_list_with_elements.size() == 3
+    assert len(linked_list_with_elements) == 3
     assert linked_list_with_elements.remove_at(99) is None
-    assert linked_list_with_elements.size() == 3
+    assert len(linked_list_with_elements) == 3
 
 
 def test_is_empty(linked_list):
@@ -160,16 +169,32 @@ def test_is_not_empty(linked_list_with_elements):
 
 
 def test_size_when_empty(linked_list):
-    assert linked_list.size() == 0
+    assert len(linked_list) == 0
 
 
 def test_size_when_not_empty(linked_list_with_elements):
-    assert linked_list_with_elements.size() == 3
+    assert len(linked_list_with_elements) == 3
 
 
-def test_to_string(linked_list_with_elements):
-    assert linked_list_with_elements.to_string() == '1,2,3'
+def test_str(linked_list_with_elements):
+    assert str(linked_list_with_elements) == "1,2,3"
 
 
-def test_to_string_when_empty(linked_list):
-    assert linked_list.to_string() == ''
+def test_str_when_empty(linked_list):
+    assert str(linked_list) == ""
+
+
+def test_str(linked_list_with_elements):
+    assert bool(linked_list_with_elements)
+
+
+def test_str_when_empty(linked_list):
+    assert not bool(linked_list)
+
+
+def test_head(linked_list_with_elements):
+    assert linked_list_with_elements.head.element == 1
+
+
+def test_head_empty(linked_list):
+    assert linked_list.head is None
